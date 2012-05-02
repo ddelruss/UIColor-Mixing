7b305a831d306eeef4ad1afc771dc04b2beca59b
Damien Del Russo <ddelruss@mac.com>
2012-05-02 16:50:23 -0400

    CMYK working.

--- UIColorMixExample/UIColor+Mixing.h
+++ UIColorMixExample/UIColor+Mixing.h
@@ -10,12 +10,28 @@
 
 @interface UIColor (Mixing)
 
-+ (UIColor*)rybColorWithRed:(CGFloat)red yellow:(CGFloat)yellow blue:(CGFloat)blue alpha:(CGFloat)alpha;
-- (void)rybGetRed:(CGFloat*)red yellow:(CGFloat*)yellow blue:(CGFloat*)blue alpha:(CGFloat*)alpha;
++ (UIColor*)rybColorWithRed:(CGFloat)red
+                     yellow:(CGFloat)yellow 
+                       blue:(CGFloat)blue 
+                      alpha:(CGFloat)alpha;
++ (UIColor*)cmykColorWithCyan:(CGFloat)cyan 
+                      magenta:(CGFloat)magenta 
+                       yellow:(CGFloat)yellow 
+                        black:(CGFloat)black 
+                        alpha:(CGFloat)alpha;
+- (void)rybGetRed:(CGFloat*)red 
+           yellow:(CGFloat*)yellow 
+             blue:(CGFloat*)blue 
+            alpha:(CGFloat*)alpha;
+- (void)cmykGetCyan:(CGFloat*)cyan 
+            magenta:(CGFloat*)magenta 
+             yellow:(CGFloat*)yellow 
+              black:(CGFloat*)black
+              alpha:(CGFloat*)alpha;
 
 + (UIColor*)rgbMixForColors:(NSArray*)arrayOfColors; // mix as light, additive. fundamentally RGB doesn't mix differently than RYB
 + (UIColor*)rybMixForColors:(NSArray*)arrayOfColors; // mix as physical material, averaging brightness
-//+ (UIColor*)cmykMixForColors:(NSArray*)arrayOfColors;
++ (UIColor*)cmykMixForColors:(NSArray*)arrayOfColors;
 
 
 
--- UIColorMixExample/UIColor+Mixing.m
+++ UIColorMixExample/UIColor+Mixing.m
@@ -106,6 +106,45 @@
     NSLog(@"rybGetRed, ending values are %f, %f, %f, %f.",*red, *yellow, *blue, *alpha);
 }
 
++ (UIColor*)cmykColorWithCyan:(CGFloat)cyan magenta:(CGFloat)magenta yellow:(CGFloat)yellow black:(CGFloat)black alpha:(CGFloat)alpha {
+    float r, g, b, c, m, y, k;
+    c = cyan;
+    m = magenta;
+    y = yellow;
+    k = black;
+    
+    r = (1-k)-(1-k)*c;
+    g = (1-k)-(1-k)*m;
+    b = (1-k)-(1-k)*y;
+
+    return [UIColor colorWithRed:r green:g blue:b alpha:alpha];
+}
+
+- (void)cmykGetCyan:(CGFloat *)cyan magenta:(CGFloat *)magenta yellow:(CGFloat *)yellow black:(CGFloat *)black alpha:(CGFloat *)alpha {
+    //conversion from http://www.codeproject.com/KB/applications/xcmyk.aspx
+    float r, g, b, c, m, y, k, a;
+    // easier to normalize when converting cmyk
+    [self getRed:&r green:&g blue:&b alpha:&a];
+
+    k = MIN(1-r, MIN(1-g, 1-b));
+    if (k < 1) {
+        c = (1-r-k)/(1-k);
+        m = (1-g-k)/(1-k);
+        y = (1-b-k)/(1-k); 
+    }
+    else {
+        c = 0;
+        m = 0;
+        y = 0;
+    }
+    //simple algorithm ensures values are within bounds, so no need for setters with controls
+    if (cyan) *cyan = c;
+    if (magenta) *magenta = m;
+    if (yellow) *yellow = y;
+    if (black) *black = k;
+    if (alpha) *alpha = a;
+}
+
 + (UIColor*)rgbMixForColors:(NSArray*)arrayOfColors {
     UIColor *resultColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
     
@@ -184,7 +223,37 @@
     return [UIColor rybColorWithRed:c1 yellow:c2 blue:c3 alpha:1.0];
 }
 
-//+ (UIColor*)cmykMixForColors:(NSArray*)arrayOfColors;
++ (UIColor*)cmykMixForColors:(NSArray*)arrayOfColors {
+    CGFloat c1, c2, c3, c4, m1, m2, m3, m4, c1Total, c2Total, c3Total, c4Total, notUsed;
+    int count = [arrayOfColors count];
+    
+    if (count == 0) return  [UIColor whiteColor];;
+    if (count == 1) return  [[arrayOfColors lastObject] copy];
+    
+    m1 = m2 = m3 = m4 = 0;
+    c1Total = c2Total = c3Total = c4Total = 0;
+    for (UIColor* aColor in arrayOfColors) {
+        [aColor cmykGetCyan:&c1 magenta:&c2 yellow:&c3 black:&c4 alpha:&notUsed];
+        
+        c1Total += c1;
+        c2Total += c2;
+        c3Total += c3;
+        c4Total += c4;
+        m1 = (c1 > m1 ? c1 : m1);
+        m2 = (c2 > m2 ? c2 : m2);
+        m3 = (c3 > m3 ? c3 : m3);
+        m4 = (c4 > m4 ? c4 : m4);        
+    }    
+    
+    c1 = c1Total/sqrt(count+1);
+    c2 = c2Total/sqrt(count+1);
+    c3 = c3Total/sqrt(count+1);
+    
+    // Black gets accumulated
+    c4 = c4Total/sqrt(sqrt(count));
+                
+    return [UIColor cmykColorWithCyan:c1 magenta:c2 yellow:c3 black:c4 alpha:1.0];
+}
 
 
 @end
